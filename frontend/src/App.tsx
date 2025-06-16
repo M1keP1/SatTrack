d4d44d96498acab23784871dd7847eb6cb7c69ac
Mihir Kumar Patel <mihirkumar654@gmail.com>
2025-06-16 22:20:16 +0200

    Added Blank Cesium Globe
    with disabled default UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import Globe from "./components/Globe";
 
 function App() {
   return (
-    <div></div>
-  )
- 
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Globe />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Globe.tsx
@@ -0,0 +1,26 @@
+import { Viewer } from "resium";
+import { Ion } from "cesium";
+import { useRef } from "react";
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwOTkzNWYyMS1mMDIzLTRhMWItYWIxZS0wZjdmM2UzZDExYTYiLCJpZCI6MzAzMzk1LCJpYXQiOjE3NDc1MTc4MzB9.xVdwgbTzZJ1XrAk3BCb2K1W4lfkEpNUfAALkXe8pElA";
+
+export default function Globe() {
+  const viewerRef = useRef(null);
+
+  return (
+    <Viewer
+      full
+      ref={viewerRef}
+      baseLayerPicker={false}
+      geocoder={false}
+      homeButton={false}
+      sceneModePicker={false}
+      navigationHelpButton={false}
+      animation={false}
+      timeline={false}
+      infoBox={false}
+      selectionIndicator={false}
+    >
+    </Viewer>
+  );
+}
